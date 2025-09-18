c5278cd33b685840adb66e47fc491624437b5f3f
Utkarsh Raghuwanshi <utkarshraghuwanshi89@gmail.com>
2025-09-19 01:08:17 +0530

    Create Module_2_Q8.cpp

---
+++ Module_2_Q8.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+#include <cctype>
+using namespace std;
+
+int evaluatePostfix(string exp) {
+    stack<int> st;
+    for (char c : exp) {
+        if (isdigit(c)) {
+            st.push(c - '0');  
+        } else {
+            int val2 = st.top(); st.pop();
+            int val1 = st.top(); st.pop();
+            switch (c) {
+                case '+': st.push(val1 + val2); break;
+                case '-': st.push(val1 - val2); break;
+                case '*': st.push(val1 * val2); break;
+                case '/': st.push(val1 / val2); break;
+            }
+        }
+    }
+    return st.top();
+}
+
+int main() {
+    string postfix;
+    cout << "Enter postfix expression: ";
+    cin >> postfix;
+    cout << "Result: " << evaluatePostfix(postfix) << endl;
+    return 0;
+}
