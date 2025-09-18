4db2ff975417eea979ee0b72858865e685509f22
Utkarsh Raghuwanshi <utkarshraghuwanshi89@gmail.com>
2025-09-19 01:03:30 +0530

    Create Module_2_Q2.cpp

---
+++ Module_2_Q2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+#define SIZE 5  
+
+class Stack {
+    int arr[SIZE];
+    int top;
+
+public:
+    Stack() {
+        top = -1; 
+    }
+
+    void push(int value) {
+        if (top == SIZE - 1) {
+            cout << "Stack Overflow! Cannot push " << value << endl;
+        } else {
+            arr[++top] = value;
+            cout << value << " pushed into stack." << endl;
+        }
+    }
+
+    void pop() {
+        if (top == -1) {
+            cout << "Stack Underflow! Cannot pop." << endl;
+        } else {
+            cout << arr[top--] << " popped from stack." << endl;
+        }
+    }
+
+    void peek() {
+        if (top == -1) {
+            cout << "Stack is empty. No top element." << endl;
+        } else {
+            cout << "Top element is: " << arr[top] << endl;
+        }
+    }
+
+    void display() {
+        if (top == -1) {
+            cout << "Stack is empty." << endl;
+        } else {
+            cout << "Stack elements: ";
+            for (int i = top; i >= 0; i--) {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    s.display();
+
+    s.peek(); 
+
+    s.pop();
+    s.peek();  
+
+    return 0;
+}
