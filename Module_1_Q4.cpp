3ac35266baaa92d0ef9312cc1890a16c01aaceb8
Utkarsh Raghuwanshi <utkarshraghuwanshi89@gmail.com>
2025-09-19 00:56:01 +0530

    Create Module_1_Q4.cpp

---
+++ Module_1_Q4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    cout << "Maximum element = " << max << endl;
+    cout << "Minimum element = " << min << endl;
+
+    return 0;
+}
