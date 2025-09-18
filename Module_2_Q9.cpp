92b7f3dc73a9dbf1741af77a8d15c5abd48bfbe1
Utkarsh Raghuwanshi <utkarshraghuwanshi89@gmail.com>
2025-09-19 01:15:38 +0530

    Create Module_2_Q9.cpp

---
+++ Module_2_Q9.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isBalanced(string expr) {
+    stack<char> st;
+    for (char c : expr) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        } else if (c == ')' || c == '}' || c == ']') {
+            if (st.empty()) return false;
+            char top = st.top(); st.pop();
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+    return st.empty();
+}
+
+int main() {
+    string expr;
+    cout << "Enter expression: ";
+    cin >> expr;
+
+    if (isBalanced(expr))
+        cout << "Balanced\n";
+    else
+        cout << "Not Balanced\n";
+
+    return 0;
+}
