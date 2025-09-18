abd9bd575df2cde2635a2ce9c0aac9e0525710f0
Utkarsh Raghuwanshi <utkarshraghuwanshi89@gmail.com>
2025-09-19 00:59:41 +0530

    Create Module_1_Q8.cpp

---
+++ Module_1_Q8.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int evenCount = 0, oddCount = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0)
+            evenCount++;
+        else
+            oddCount++;
+    }
+
+    cout << "Total even numbers = " << evenCount << endl;
+    cout << "Total odd numbers  = " << oddCount << endl;
+
+    return 0;
+}
