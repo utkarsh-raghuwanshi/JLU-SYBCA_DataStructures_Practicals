f8899b868cd9e5cc64fe9ef319b8701e793b8348
Utkarsh Raghuwanshi <utkarshraghuwanshi89@gmail.com>
2025-09-19 00:55:25 +0530

    Create Module_1_Q3.cpp

---
+++ Module_1_Q3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    cout << "Sum of all elements = " << sum << endl;
+
+    return 0;
+}
