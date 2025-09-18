29d70b618eb38066b8df9b0b83c6cb10db93f290
Utkarsh Raghuwanshi <utkarshraghuwanshi89@gmail.com>
2025-09-19 01:00:11 +0530

    Create Module_1_Q9.cpp

---
+++ Module_1_Q9.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr1[n], arr2[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr2[i] = arr1[i];
+    }
+
+    cout << "Copied array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr2[i] << " ";
+    }
+
+    return 0;
+}
