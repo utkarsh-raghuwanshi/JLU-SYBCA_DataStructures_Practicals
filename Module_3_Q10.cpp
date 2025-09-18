5fd64b0e701c6c1062fc1cb47f1cdeaaa8da7ab6
Utkarsh Raghuwanshi <utkarshraghuwanshi89@gmail.com>
2025-09-19 01:09:38 +0530

    Create Module_3_Q10.cpp

---
+++ Module_3_Q10.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node* head = nullptr;
+
+void insertAtEnd(int value) {
+    Node* newNode = new Node;
+    newNode->data = value;
+    newNode->next = nullptr;
+
+    if (head == nullptr) {
+        head = newNode;
+    } else {
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+int countNodes() {
+    int count = 0;
+    Node* temp = head;
+    while (temp != nullptr) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+void displayList() {
+    Node* temp = head;
+    if (head == nullptr) {
+        cout << "List is empty\n";
+        return;
+    }
+    cout << "Linked List: ";
+    while (temp != nullptr) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL\n";
+}
+
+int main() {
+    insertAtEnd(10);
+    insertAtEnd(20);
+    insertAtEnd(30);
+
+    displayList();
+    cout << "Number of nodes: " << countNodes() << endl;
+
+    insertAtEnd(40);
+    displayList();
+    cout << "Number of nodes: " << countNodes() << endl;
+
+    return 0;
+}
